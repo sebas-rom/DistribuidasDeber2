ac0abc59b06bda3e5dd87710b8e35dae94033abd
Juan505 <juankrgro808@gmail.com>
2024-03-18 21:29:36 -0500

    Changes

--- src/main/java/com/userwebsocket/UserBean.java
+++ src/main/java/com/userwebsocket/UserBean.java
@@ -2,6 +2,8 @@ package com.userwebsocket;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.faces.push.Push;
@@ -10,13 +12,10 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-//@formatter:off
-
 @ViewScoped
 @Named("userBean")
 public class UserBean implements Serializable {
 
-    // Push more channels if we need more chatrooms
     @Inject
     @Push(channel = "chatChannel")
     PushContext chatChannel;
@@ -24,39 +23,54 @@ public class UserBean implements Serializable {
     @Inject
     @Push(channel = "chatChannel2")
     PushContext chatChannel2;
+
     private static final Logger LOG = Logger.getLogger(UserBean.class.getName());
     private String currentUser;
     private String currentMessage;
-
-
+    private List<String> chatChannelMessages = new ArrayList<>();
+    private List<String> chatChannel2Messages = new ArrayList<>();
 
     public void sendMessage(String message, String user, String channel) {
-    setCurrentUser(user);
-    LocalDateTime now = LocalDateTime.now();
-    String formattedMessage = String.format(" [%02d:%02d:%02d] %s: %s", now.getHour(), now.getMinute(), now.getSecond(), currentUser, message);
-    // select appropiate channel to send the message
-    // assure that the function call on chat.xhtml passes the correct channel string
-    if ("chatChannel".equals(channel)) {
-        chatChannel.send(formattedMessage);
-    } else if ("chatChannel2".equals(channel)) {
-        chatChannel2.send(formattedMessage);
+        setCurrentUser(user);
+        LocalDateTime now = LocalDateTime.now();
+        String formattedMessage = String.format("[%02d:%02d:%02d] %s: %s", now.getHour(), now.getMinute(), now.getSecond(), currentUser, message);
+
+        if ("chatChannel".equals(channel)) {
+            chatChannelMessages.add(formattedMessage);
+            chatChannel.send(formattedMessage);
+        } else if ("chatChannel2".equals(channel)) {
+            chatChannel2Messages.add(formattedMessage);
+            chatChannel2.send(formattedMessage);
+        }
+
+        System.out.println(formattedMessage);
+        setCurrentMessage(""); // Clear the currentMessage
+
+
     }
-    System.out.println(formattedMessage);
-//    System.out.println(" ->"+ channel); // for debugging
-    setCurrentMessage(""); // Clear the currentMessage
+
+    public List<String> getChatChannelMessages() {
+        return chatChannelMessages;
+    }
+
+    public List<String> getChatChannel2Messages() {
+        System.out.println("Loading messages.....");
+        return chatChannel2Messages;
     }
 
     public String getCurrentUser() {
-        return this.currentUser;
+        return currentUser;
     }
-    public void setCurrentUser(String currentUser){
-        this.currentUser=currentUser;
+
+    public void setCurrentUser(String currentUser) {
+        this.currentUser = currentUser;
     }
 
     public String getCurrentMessage() {
-        return this.currentMessage;
-    }public void setCurrentMessage(String currentMessage) {
-        this.currentMessage = currentMessage;
+        return currentMessage;
     }
 
+    public void setCurrentMessage(String currentMessage) {
+        this.currentMessage = currentMessage;
+    }
 }
